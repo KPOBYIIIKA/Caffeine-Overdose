aa83c4506a620d9ce82e7cd0a4ec4b95432b6b01
cxdlx <cxclx@outlook.com>
2023-04-17 18:16:41 +0200

    Added FontAwesome

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
+import classes from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function Header() {
     return (
-        <header>
-            <h1>Fullfocus</h1>
+        <header className={classes}>
+            <FontAwesomeIcon icon='coffee' className={classes.fa}></FontAwesomeIcon>
+            <h1>Caffeine Overdose</h1>
             <nav>
 
             </nav>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheckSquare, faCoffee);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
