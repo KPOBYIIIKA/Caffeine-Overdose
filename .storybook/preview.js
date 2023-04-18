1be7addb92b0d3662b1568faaf1cd1c10355ba8d
Lean <cxclx@outlook.com>
2023-04-19 01:49:25 +0200

    Stories cleanup & Header changes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
 /** @type { import('@storybook/react').Preview } */
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheckSquare, faCoffee);
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
