f8ef9aa6cd7d84d2645e4f3f89fb6ed0ef59cf5d
Lean <cxclx@outlook.com>
2023-04-20 18:32:47 +0200

    Added Button Click Sound

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,19 @@
 import styles from './Button.module.scss'
+import clickSound from '../../assets/sounds/click.wav'
 
 function Button(props) {
+    const playSound = () => {
+        const audio = new Audio(clickSound);
+        audio.play();
+    };
+
+    const handleClick = () => {
+        playSound();
+        props.onClick();
+    };
+
     return (
-        <button className={`${styles.button} ${props.isRunning ? styles.active : ''}`} onClick={props.onClick}>{props.label}</button>
+        <button className={`${styles.button} ${props.isRunning ? styles.active : ''}`} onClick={handleClick}>{props.label}</button>
     )
 }
 
