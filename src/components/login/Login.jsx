4e8f02b639a0387c9198b9b92cb95b800bf1959a
cxdlx <cxclx@outlook.com>
2023-04-21 18:25:26 +0200

    Login form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,7 +3,17 @@ import styles from './Login.module.scss'
 function Login() {
     return(
         <div className={styles}>
-            
+            <form>
+                <div>
+                    <label>Email</label>
+                    <input/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input/>
+                </div>
+                <button type='submit'>Log In</button>
+            </form>
         </div>
     )
 }
